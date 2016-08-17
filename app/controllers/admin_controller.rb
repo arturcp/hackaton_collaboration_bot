111e963f3601f3af7187c599c1f3be7dc8fab59d
Artur <artur.prado@gmail.com>
2016-08-16 22:11:20 -0300

    Include simple security authentication in admin panel

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,7 @@
 class AdminController < ApplicationController
+  http_basic_authenticate_with name: ENV.fetch('ADMIN_USER'),
+    password: ENV.fetch('ADMIN_PASSWORD') unless Rails.env.test?
+
   def index
     @houses = House.all
     @points = @houses.reduce(0) { |sum, house| sum += house.points }
